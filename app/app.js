2aa2a39dee806dbe50216be5bc44ad06201f4410
vavimayor159 <vavimayor159@gmail.com>
2016-10-08 13:22:23 -0500

    Adding chat component to the app

--- app/app.js
+++ app/app.js
@@ -2,6 +2,20 @@
  * Created by ivan on 8/10/16.
  */
 
+var ChatComponent = React.createClass({
+    render: function () {
+        return (
+            <form id="chat">
+                <input type="text" name="message" id="message"/>
+                <button className="btn waves-effect waves-light" type="submit" name="action">
+                    Enviar
+                    <i className="material-icons right">send</i>
+                </button>
+            </form>
+        )
+    }
+});
+
 var TitleComponent = React.createClass({
     render: function () {
         return (
@@ -13,10 +27,14 @@ var TitleComponent = React.createClass({
 var AppComponent = React.createClass({
     render: function () {
         return (
-            <TitleComponent />
+            <main>
+                <TitleComponent />
+                <ChatComponent />
+            </main>
         )
     }
 });
+
 ReactDOM.render(
     <AppComponent />,
     document.getElementById('app')
