e2b3e07ef27a776363483b49d1869f47b260abdb
vavimayor159 <vavimayor159@gmail.com>
2016-10-08 11:50:05 -0500

    Basic App Markup established

--- app/app.js
+++ app/app.js
@@ -1,7 +1,23 @@
 /**
  * Created by ivan on 8/10/16.
  */
+
+var TitleComponent = React.createClass({
+    render: function () {
+        return (
+            <h1>Chat con React</h1>
+        )
+    }
+});
+
+var AppComponent = React.createClass({
+    render: function () {
+        return (
+            <TitleComponent />
+        )
+    }
+});
 ReactDOM.render(
-<h1>Hello, world!</h1>,
+    <AppComponent />,
     document.getElementById('app')
 );
