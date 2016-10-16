00169d74c70682cd800e0f1d08e22dbc92ebf3bb
vavimayor159 <vavimayor159@gmail.com>
2016-10-15 22:48:08 -0500

    Message box scroll fixed and basic firebase config added

--- app/Chat/ChatApp.js
+++ app/Chat/ChatApp.js
@@ -10,6 +10,22 @@ var Chat = require('./Chat');
 
 // Create the main component
 module.exports = React.createClass({
+    /** Before the component of the app will be mounted we do all the necesary
+    configuration */
+    componentWillMount: function () {
+
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyBijCbjzyaLxlWI6r-wAPj08MF2qZTvmmQ",
+            authDomain: "chat-react-16ae7.firebaseapp.com",
+            databaseURL: "https://chat-react-16ae7.firebaseio.com",
+            storageBucket: "chat-react-16ae7.appspot.com",
+            messagingSenderId: "916238918422"
+        };
+        firebase.initializeApp(config);
+
+    },
+
     render: function () {
         return (
             <main>
